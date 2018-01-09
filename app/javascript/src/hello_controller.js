9f2b43b220c2f3d1bb79b5af4adc4961ac9193f8
yiyizym <zhuyimin@meizu.com>
2018-01-09 17:39:20 +0800

    first init

---
+++ app/javascript/src/hello_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "stimulus"
+export default class extends Controller {
+    greet() {
+        console.log(`Hello, ${this.name}!!`)
+    }
+
+    get name() {
+        return this.inputElement.value
+    }
+
+    get inputElement() {
+        return this.targets.find("name")
+    }
+}
